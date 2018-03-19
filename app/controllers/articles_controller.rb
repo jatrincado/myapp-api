94dbdae412758f05f926cbde1b9e8e1cc694d5b1
Jorge Trincado <jatrincado@uc.cl>
2018-03-19 10:37:10 -0300

    Added user devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,21 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @articles = Article.all
   end
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
-    @article = Article.new
+    @article = current_user.articles.build
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
     if @article.save
       redirect_to @article
     else
@@ -24,7 +24,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -33,12 +32,15 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
 private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :summary, :body, :link)
   end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
+  belongs_to :user
   validates :title, presence: true
   validates :summary, presence: true, length: { maximum: 200 }
   validates :body, presence: true
