2507b40d6f24d5b8ff84ca7c2cf6fde8e00cc1dd
Jorge Trincado <jatrincado@uc.cl>
2018-03-19 09:37:47 -0300

    Added comments controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments, dependent: :destroy
   validates :title, presence: true
   validates :summary, presence: true, length: { maximum: 200 }
   validates :body, presence: true
